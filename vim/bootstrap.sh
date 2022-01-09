a3556aee3f7349ee0a17eda79ff6b851ce0bc6a5
DrewDalmedo <andrewdalmedo@gmail.com>
2022-01-09 17:34:05 -0500

    Add macOS / homebrew support (warning - untested

--- vim/bootstrap.sh
+++ vim/bootstrap.sh
@@ -9,8 +9,18 @@ getVundle() {
 }
 
 installDependenciesDeb() {
+    echo "Installing basic dependencies (vim, git, etc)..."
+    (sudo apt install vim git -y -qq &>/dev/null) || (echo "Error: could not install dependencies! Exiting script..."; exit 0)
     echo "Installing YouCompleteMe dependencies..."
     (sudo apt install build-essential cmake vim-nox python3-dev mono-complete golang nodejs default-jdk npm -y -qq &>/dev/null) || (echo "Error: could not install dependencies! Exiting script..."; exit 0)
+}
+
+installDependenciesBrew() {
+    echo "Installing basic dependencies (vim, git, etc)..."
+    (brew install git vim &>/dev/null) || (echo "Error: could not install dependencies! Exiting script..."; exit 0)
+    echo "Installing YouCompleteMe dependencies..."
+    (brew install cmake python go nodejs java) || (echo "Error: could not install dependencies! Exiting script..."; exit 0)
+    (sudo ln -sfn /usr/local/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk) || (echo -e "Could not link Java successfully. Try running this command:\nsudo ln -sfn /usr/local/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk"; exit 0)
 }
 
 compileYCM() {
@@ -19,8 +29,11 @@ compileYCM() {
     echo "YouCompleteMe successfully installed!"
 }
 
-# main part of the program
+# start of the script
 echo "Bootstrapping vim..."
+
+# install dependencies
+(which apt &>/dev/null && installDependenciesDeb) || (which brew &>/dev/null && installDependenciesBrew) || (echo "Error: System is not suppported, could not install dependencies. Exiting script..."; exit 0)
 
 # check if vim is installed
 which vim &>/dev/null || (echo "Error: vim is not installed! Exiting script..."; exit 0)
@@ -28,8 +41,7 @@ which vim &>/dev/null || (echo "Error: vim is not installed! Exiting script...";
 # get vundle and install the plugins
 (which git &>/dev/null && ln ./.vimrc ~/.vimrc; getVundle) || (echo "Error: command 'git' not found! Exiting script..."; exit 0)
 
-# compile YouCompleteMe
-(which apt &>/dev/null && installDependenciesDeb) || (echo "Error: System is not deb-based, could not install YCM dependencies."; exit 0)
+# compile YouCompleteMe (YCM)
 compileYCM 
 
 echo ""
