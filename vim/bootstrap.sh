bb4060ddfb0f060928433a2b2f06916835865f41
DrewDalmedo <andrewdalmedo@gmail.com>
2022-01-09 11:37:05 -0500

    Compile YCM and output a done message

--- vim/bootstrap.sh
+++ vim/bootstrap.sh
@@ -8,6 +8,14 @@ getVundle() {
     echo "Vundle successfully installed!"
 }
 
+installDependenciesDeb() {
+    echo "Installing YouCompleteMe dependencies..."
+    apt install build-essential cmake vim-nox python3-dev mono-complete golang nodejs default-jdk npm -y &>/dev/null || (echo "Error: could not install dependencies! Exiting script..."; exit 0)
+    echo "Compiling YouCompleteMe..."
+    python3 ~/.vim/bundle/YouCompleteMe/install.py --all &>/dev/null
+    echo "YouCompleteMe successfully installed!"
+}
+
 # main part of the program
 echo "Bootstrapping vim..."
 
@@ -17,3 +25,8 @@ which vim &>/dev/null || (echo "Error: vim is not installed! Exiting script...";
 # get vundle and install the plugins
 (which git &>/dev/null && ln ./.vimrc ~/.vimrc; getVundle) || (echo "Error: command 'git' not found! Exiting script..."; exit 0)
 
+# compile YouCompleteMe
+(which apt &>/dev/null && installDependenciesDeb) || (echo "Error: System is not deb-based, could not install YCM dependencies."; exit 0)
+
+echo "\n"
+echo "Done! Enjoy your new vim config!"
